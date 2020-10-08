8b9759396e3839d4e16da44b415539f55e2a08b9
utkarshmega <utkarsheduc@gmail.com>
2020-10-09 00:36:19 +0530

    Welcome to stack and queue implementation

---
+++ src/com/training/stackandqueueusinglinkedlist/StackUsingLinkedList.java
@@ -0,0 +1,10 @@
+package com.training.stackandqueueusinglinkedlist;
+
+public class StackUsingLinkedList<T> {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Implementation of stack and queue using linked list");
+
+	}
+
+}
